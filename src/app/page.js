904559aa48077889e46f181a9ca943295452cae6
nbrucepro <nezabruce@gmail.com>
2024-11-30 13:27:00 +0800

    deployment

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import NBAPage from "./nba/page";
+import View from "../components/NBA/views/Home";
 
 export const metadata = {
   title: "Kepler Sports",
 };
 
 export default async function Page() {
-    return <div>Hello</div>;
+  return <View />;
 }
